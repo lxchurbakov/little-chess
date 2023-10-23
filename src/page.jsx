a0645a17bce980a6a85b300bec3600c734adc6e1
lxchurbakov <lxchurbakov@gmail.com>
2023-10-23 18:01:19 +0300

    Draft empty page

---
+++ src/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Base, Container, Text } from './components/atoms';
+
+export default () => {
+    return (
+        <Container>
+            <Base p="64px 0">
+                <Text mb="24px" size="32px" weight="800">Little Chess</Text>
+
+                <Text size="16px" weight="400">Здесь мы будем творить создавать нашу шахматную доску.</Text>
+            </Base>
+        </Container>
+    ); 
+};
