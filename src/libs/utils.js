c653dd87c1f7cdbf16d891c64cf154ee699b260e
lxchurbakov <lxchurbakov@gmail.com>
2023-11-02 08:45:32 +0300

    Integrate toghether

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -4,20 +4,30 @@ export const createid = () => _id++;
 export const parsefen = (v) => {
     let x = 0;
     let y = 0;
+
     let result = [];
 
-    v.split('').forEach((value) => {
-        if ('rnbqkpRNBQKP'.includes(value) && x < 8 && y < 8) {
-            result.push({ id: createid(), position: { x, y }, code: value });
-        }
-
-        if (value === '/') {
+    for (let value of v.split('')) {
+        if ('12345678'.includes(value)) {
+            x += Number(value);
+        } else if ('rnbqkpRNBQKP'.includes(value)) {
+            result.push({ id: createid(), position: { x: x++, y }, code: value });
+        } else if (value === '/') {
             x = 0;
             y++;
-        } else {
-            x++;
+        } if (value === ' ') {
+            break;
         }
-    });
+    }
+
+    // v.split('').forEach((value) => {
+        
+    // });
 
     return result;
 };
+
+export const DEFAULT_POSITION = 
+    'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+// 3QQ1Q1/k7/8/1Q6/5Q2/pP6/P7/2R3K1 b - - 0 50
--- src/page.jsx
+++ src/page.jsx
@@ -1,327 +1,77 @@
 import React from 'react';
 
 import { Circle, Clickable, Relative, Absolute, Image, Base, Container, Text } from './components/atoms';
-import { parsefen, createid } from './libs/utils';
+
 import { board, PIECES } from './libs/images';
+import { DEFAULT_POSITION,  } from './libs/utils';
+import { Chess, useChess, isWhite, match } from './libs/chess';
 
-const match = (a, b) => a.x === b.x && a.y === b.y;
-const add = (a, b) => ({ x: a.x + b.x, y: a.y + b.y });
-const isWhite = ({code}) => 'QNRBPK'.includes(code);
-const isPositionValid = (p) => p.x > -1 && p.y > -1 && p.x < 8 && p.y < 8;
+import { Drag, Drop } from './libs/dnd';
 
-const INITPOS = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-
-// const movesForBishop = (start)
-
-const useChess = () => {
-    const [white, setWhite] = React.useState(true);
-    const [pieces, setPieces] = React.useState([]);
-
-    const find = React.useCallback((p) => {
-        return pieces.find(($) => match($.position, p));
-    }, [pieces]);
-
-    const get = React.useCallback((id) => {
-        return pieces.find(($) => $.id === id);
-    }, [pieces]);
-
-    const load = React.useCallback((fen) => {
-        setPieces(parsefen(fen));
-    }, []);
-
-    const move = React.useCallback((id, newposition) => {
-        const piece = find(newposition);
-        const pieceToMove = get(id);
-
-        if (piece && isWhite(piece) !== isWhite(pieceToMove)) {
-            // throw new Error(`cannot_move_here`);
-            setPieces(($) => {
-                return $.filter(($$) => {
-                    return $$.id !== piece.id;
-                });
-            });
-        }
-
-        setPieces(($) => {
-            return $.map(($$) => {
-                if ($$.id === id) {
-                    return { ...$$, position: newposition };
-                } else {
-                    return $$;
-                }
-            });
-        });
-
-        setWhite(($) => !$);
-    }, [setWhite, find, pieces, setPieces]);
-
-    // const can = React.useCallback((id, p) => {
-    //     const piece = get(id);
-
-    //     if (!piece) {
-    //         throw new Error(`piece_not_found`);
-    //     }
-
-    //     if (piece.code.toLowerCase() === 'r') {
-    //         return piece.position.x === p.x || piece.position.y === p.y;
-    //     }
-    // }, [get]);
-
-    const moves = React.useCallback((id) => {
-        const piece = pieces.find(($) => $.id === id);
-
-        const check = (position) => {
-            if (!isPositionValid(position)) {
-                return false;
-            }
-
-            const pieceInPosition = find(position);
-
-            if (!pieceInPosition) {
-                return 'move';
-            } else {
-                if (isWhite(pieceInPosition) === isWhite(piece)) {
-                    return false;
-                } else {
-                    return 'capture';
-                }
-            }
-        };
-
-        const walk = (position, predicate) => {
-            let result = [];
-    
-            for (let i = 0; i < 8; ++i) {
-                position = predicate(position);
-
-                const checkResult = check(position);
-
-                if (checkResult === false) {
-                    break;
-                } else if (checkResult === 'capture') {
-                    result.push({ position: { ...position }, type: 'capture' });
-                    break
-                } else if (checkResult === 'move') {
-                    result.push({ position: { ...position }, type: 'move' });
-                }
-            }
-    
-            return result;
-        };
-
-        if (piece.code.toLowerCase() === 'b') {
-            return [
-                ...walk({ ...piece.position }, (p) => ({ x: p.x - 1, y: p.y - 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x - 1, y: p.y + 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x + 1, y: p.y + 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x + 1, y: p.y - 1 })),
-            ];
-        }
-
-        if (piece.code.toLowerCase() === 'r') {
-            return [
-                ...walk({ ...piece.position }, (p) => ({ x: p.x, y: p.y - 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x, y: p.y + 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x - 1, y: p.y })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x + 1, y: p.y })),
-            ];
-        }
-    
-        if (piece.code.toLowerCase() === 'q') {
-            return [
-                ...walk({ ...piece.position }, (p) => ({ x: p.x - 1, y: p.y - 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x - 1, y: p.y + 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x + 1, y: p.y + 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x + 1, y: p.y - 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x, y: p.y - 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x, y: p.y + 1 })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x - 1, y: p.y })),
-                ...walk({ ...piece.position }, (p) => ({ x: p.x + 1, y: p.y })),
-            ];
-        }
-    
-        const flatten = (a) => a.reduce((acc, group) => acc.concat(group), []);
-        const zip = (a, b) => flatten(a.map((va) => b.map((vb) => [va, vb])));
-
-        if (piece.code.toLowerCase() === 'n') {
-            return [
-                ...zip([2, -2], [-1, 1]).map(([x, y]) => ({ x, y })),
-                ...zip([-1, 1], [2, -2]).map(([x, y]) => ({ x, y })),
-            ]
-                .map((p) => add(p, piece.position))
-                .map((p) => check(p) && ({ position: p, type: check(p) }))
-                .filter(Boolean);
-        }
-    
-        if (piece.code.toLowerCase() === 'p') {
-            const canDoTwo = (piece.code === 'p' && piece.position.y === 1) 
-                || (piece.code === 'P' && piece.position.y === 6);
-            
-            const direction = piece.code === 'P' ? -1 : 1;
-    
-            const opposites = [-1, 1]
-                .map((x) => find({ x: x + piece.position.x, y: piece.position.y + direction }))
-                .filter(Boolean)
-                .map((p) => ({ ...p.position, canTake: true }));
-
-            return [
-                { x: piece.position.x, y: piece.position.y + direction, canTake: false },
-                canDoTwo ? { x: piece.position.x, y: piece.position.y + direction * 2, canTake: false } : null,
-                ...opposites,
-            ].filter(Boolean).map((p) => {
-                const checkResult = check(p);
-
-                if (checkResult) {
-                    if (checkResult === 'capture' && !p.canTake) {
-                        return null;
-                    }
-                    
-                    return { position: p, type: checkResult };
-                }
-            }).filter(Boolean)
-            ;
-        }
-    
-        if (piece.code.toLowerCase() === 'k') {
-            const options = [
-                { x: -1, y: -1 },
-                { x: -1, y: 0 },
-                { x: -1, y: 1 },
-                { x: 0, y: -1 },
-                // { x: 0, y: 0 },
-                { x: 0, y: 1 },
-                { x: 1, y: -1 },
-                { x: 1, y: 0 },
-                { x: 1, y: 1 },
-            ];
-            
-            return options.map((p) => add(p, piece.position)).map((p) => {
-                const checkResult = check(p);
-
-                if (checkResult) {
-                    return { position: p, type: checkResult };
-                }
-            })
-            .filter(Boolean);
-        }
-
-        return [];
-        // return [{ position: x: piece.position.x, y: piece.position.y - 1 }];
-    }, [find, pieces]);
-
-    const check = React.useMemo(() => {
-        return ['K', 'k'].map((code) => {
-            const king = pieces.find(($) => $.code === code);
-
-            for (let piece of pieces) {
-                const pieceMoves = moves(piece.id);
-                const threat = pieceMoves.find((move) => match(move.position, king.position));
-                
-                if (threat) {
-                    return code;
-                }
-            }
-
-            return null;
-        }).filter(Boolean).map((code) => isWhite({ code }) ? 'white' : 'black')[0];
-    }, [moves, pieces]);
-
-    console.log(check)
-
-    return React.useMemo(() => ({ 
-        pieces, move, get, find, load, move, moves, white, check
-    }), [pieces, move, get, find, move, load, moves, white, check]);
+const STOP_DRAG = (e) => {
+    e.preventDefault();
+    return false;
 };
 
-
-const Chess = ({ ...props }) => {
-    const { moves, load, pieces, move, white } = useChess();
-
-    React.useEffect(() => load(INITPOS), []);
-
-    const [highlight, setHightlight] = React.useState([]);
-
-    const start = (id) => (e) => {
-        const { clientX: x, clientY: y } = e;
-
-        const node = e.target;
-        const parent = e.target.parentElement.parentElement.parentNode;
-        const rect = parent.getBoundingClientRect();
-        const grid = rect.width / 8;
-        const start = { x, y };
-
-        // TODO export somehow paths where can be moved
-        const options = moves(id);
-        setHightlight([...options]);
-
-        // console.log({options})
-
-        // console.log(rect, grid)
-
-        const mousemove = ({ clientX: x, clientY: y }) => {
-            const offsetx = x - start.x;
-            const offsety = y - start.y;
-
-            node.style.transform = `translateX(${offsetx}px) translateY(${offsety}px)`;
-        };
-
-        const mouseup = ({ clientX: x, clientY: y }) => {
-            // TODO check if can be moved here
-            const newx = Math.floor((x - rect.left) / grid);
-            const newy = Math.floor((y - rect.top) / grid);
-
-            if (!!options.find(($) => {
-                return $.position.x === newx && $.position.y === newy;
-            })) {
-                move(id, {
-                    x: newx,
-                    y: newy,
-                });
-            }      
-            
-            setHightlight([])
-
-            node.style.transform = `none`;
-
-            window.removeEventListener('mousemove', mousemove);
-            window.removeEventListener('mouseup', mouseup);
-        };
-
-        window.addEventListener('mousemove', mousemove);
-        window.addEventListener('mouseup', mouseup);
-
-        const piece = pieces.find(($) => $.id === id);
-
-        if (isWhite(piece) && !white) {
-            mouseup(e);
-        } else if (!isWhite(piece) && white) {
-            mouseup(e);
-        } else if (options.length === 0) {
-            mouseup(e)
-        }
-    };
-
-    // console.log(highlight)
+const Piece = React.memo(({ code }) => {
+    const src = React.useMemo(() => PIECES[code], [code]);
 
     return (
-        <Relative mw="700px" w="100%" {...props}>
+        <Image w="100%" onDragStart={STOP_DRAG} src={src} />
+    );
+});
+
+const DraggablePiece = ({ id, position, code, onStart }) => {
+    const grid = `(100% / 8)`;
+
+    const handleStart = React.useCallback(() => {
+        return onStart?.(id);
+    }, [onStart, id]);
+
+    return (
+        <Drag 
+            data={id}
+            w={`calc${grid}`} 
+            left={`calc(${grid} * ${position.x})`}
+            top={`calc(${grid} * ${position.y})`}
+            style={{ zIndex: 2 }}
+            onStart={handleStart}
+        >
+            <Piece code={code} />
+        </Drag>
+    )
+};
+
+const ChessBoardView = ({ pieces, circles, onStart, onEnd, ...props }) => {
+    const [grid, setGrid] = React.useState(1);
+    const dropRef = React.useRef(null);
+
+    React.useEffect(() => {
+        setGrid(dropRef.current?.getBoundingClientRect().width / 8);
+    }, []);
+
+    const handleDrop = React.useCallback((data, { x, y }) => {
+        onEnd?.(data, {
+            x: Math.floor(x / grid),
+            y: Math.floor(y / grid),
+        });
+    }, [grid, onEnd]);
+
+    return (
+        <Drop ref={dropRef} mw="700px" w="100%" onDrop={handleDrop} {...props}>
             <Image w="100%" src={board} />
 
-            {pieces.map((piece, index) => (
-                <Absolute 
-                    key={index}
-                    w="calc(100% / 8)" 
-                    left={`calc((100% / 8) * ${piece.position.x})`}
-                    top={`calc((100% / 8) * ${piece.position.y})`}
-                    style={{ zIndex: 2 }}
-                >
-                    <Clickable onMouseDown={start(piece.id)}>
-                        <Image w="100%" onDragStart={e => e.preventDefault()} src={PIECES[piece.code]} />
-                    </Clickable>
-                </Absolute>
+            {pieces.map(({ code, position, id }) => (
+                <DraggablePiece
+                    key={id}
+                    id={id}
+                    onStart={onStart}
+                    position={position}
+                    code={code}
+                />
             ))}
 
-            {highlight.map(({ position: h }, index) => (
+            {circles.map(({ position: h }, index) => (
                 <Absolute 
                     key={index}
                     w="calc(100% / 8)" 
@@ -332,8 +82,74 @@ const Chess = ({ ...props }) => {
                     <Circle w="24px" h="24px" style={{ background: '#9453c9' }} />
                 </Absolute>
             ))}
-        </Relative>
+        </Drop>
     );
+};
+
+const chess = new Chess();
+
+const ChessBoard = () => {
+    const forceUpdate = (([, setState]) => () => setState(($) => !$))(React.useState(false));
+
+    React.useEffect(() => {
+        chess.onUpdate = forceUpdate;
+    }, [forceUpdate]);
+
+    // Store the game object itself (expressed via hook)
+    // const chess = useChess();
+    // Store places we should put our highlighs to
+    const [moves, setMoves] = React.useState([]);
+    // Figure out who is to move
+    // const [whiteToMove, setWhiteToMove] = React.useState(true);
+
+    // Load default position on startup
+    React.useEffect(() => {
+        // chess.load(DEFAULT_POSITION);
+        chess.load(`3QQ1Q1/k7/8/1Q6/5Q2/pP6/P7/2R3K1 b - - 0 50`);
+        // setWhiteToMove(true);
+    }, []);
+
+    const start = React.useCallback((id) => {
+        const piece = chess.piece(id);
+        const possibleMoves = chess.movesById(id);
+
+        console.log(possibleMoves)
+
+        // if (isWhite(piece) !== whiteToMove) {
+        //     return false;
+        // }
+
+        if (possibleMoves.length === 0) {
+            return false;
+        }
+
+        setMoves(possibleMoves);     
+
+        return true;
+    }, [setMoves, chess]);
+
+    console.log(chess.isCheck());
+    console.log(chess.isGameOver());
+
+    const end = React.useCallback((id, { x, y }) => {
+        const possibleMove = moves.find(($) => match($.position, { x, y }));
+
+        if (!!possibleMove) {
+            chess.apply(possibleMove);
+        }
+
+        setMoves([]);
+        // setWhiteToMove(($) => !$);
+    }, [moves, setMoves, chess]);
+
+    return (
+        <ChessBoardView
+            pieces={chess.pieces}
+            circles={moves}
+            onStart={start}
+            onEnd={end}
+        />
+    );  
 };
 
 export default () => {
@@ -341,10 +157,9 @@ export default () => {
         <Container>
             <Base p="64px 0">
                 <Text mb="12px" size="32px" weight="800">Little Chess</Text>
-
                 <Text mb="48px" size="16px" weight="400">Здесь мы будем творить создавать нашу шахматную доску.</Text>
 
-                <Chess />
+                <ChessBoard />
             </Base>
         </Container>
     ); 
