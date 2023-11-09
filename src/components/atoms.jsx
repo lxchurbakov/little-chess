be34ea2ba4b68c92fa8e75f4bd5294ff2f161c66
lxchurbakov <lxchurbakov@gmail.com>
2023-11-09 12:06:10 +0300

    Import assets and draw the board

--- src/components/atoms.jsx
+++ src/components/atoms.jsx
@@ -4,6 +4,10 @@ export const Base = styled.div`
     padding: ${props => props.p};
     margin: ${props => props.m};
     margin-bottom: ${props => props.mb};
+    width: ${props => props.w};
+    height: ${props => props.h};
+    max-width: ${props => props.mw};
+    max-height: ${props => props.mh};
 `;
 
 export const Container = styled(Base)`
@@ -13,8 +17,28 @@ export const Container = styled(Base)`
 `;
 
 export const Text = styled(Base)`
-    font-family: Raleway, sans;
+    font-family: Raleway, sans-serif;
     font-size: ${props => props.size || '16px'};
     font-weight: ${props => props.weight || '400'};
     color: ${props => props.color || '#333333'};
+    text-align: ${props => props.align};
+`;
+
+export const Image = styled.img`
+    padding: ${props => props.p};
+    margin: ${props => props.m};
+    margin-bottom: ${props => props.mb};
+    width: ${props => props.w};
+`;
+
+export const Relative = styled(Base)`
+    position: relative;
+`;
+
+export const Absolute = styled(Base)`
+    position: absolute;
+    top: ${props => props.top};
+    left: ${props => props.left};
+    right: ${props => props.right};
+    bottom: ${props => props.bottom};
 `;
--- src/page.jsx
+++ src/page.jsx
@@ -1,6 +1,62 @@
 import React from 'react';
 
-import { Base, Container, Text } from './components/atoms';
+import { Relative, Absolute, Base, Container, Text, Image } from './components/atoms';
+
+import board from './assets/board.png';
+
+import bn from './assets/bn.png';
+import wn from './assets/wn.png';
+import bb from './assets/bb.png';
+import bk from './assets/bk.png';
+import bp from './assets/bp.png';
+import bq from './assets/bq.png';
+import br from './assets/br.png';
+import wb from './assets/wb.png';
+import wk from './assets/wk.png';
+import wp from './assets/wp.png';
+import wq from './assets/wq.png';
+import wr from './assets/wr.png';
+
+export const PIECES = {
+    N: wn,
+    n: bn,
+    B: wb,
+    b: bb,
+    K: wk,
+    k: bk,
+    P: wp,
+    p: bp,
+    Q: wq,
+    q: bq,
+    R: wr,
+    r: br,
+};
+
+const ChessBoard = ({ ...props }) => {
+    const grid = `(100% / 8)`;
+
+    const [pieces, setPieces] = React.useState([
+        { code: 'q', position: { x: 2, y: 1 } },
+        { code: 'N', position: { x: 3, y: 1 } },
+    ]);
+
+    return (
+        <Relative {...props}>
+            <Image w="100%" src={board} />
+
+            {pieces.map((piece) => (
+                <Absolute
+                    w={`calc${grid}`} 
+                    left={`calc(${grid} * ${piece.position.x})`}
+                    top={`calc(${grid} * ${piece.position.y})`}
+                    style={{ zIndex: 2 }}
+                >
+                    <Image w="100%" src={PIECES[piece.code]} />
+                </Absolute>
+            ))}
+        </Relative>
+    );
+};
 
 export default () => {
     return (
@@ -10,9 +66,11 @@ export default () => {
                     Little Chess
                 </Text>
 
-                <Text size="16px" weight="400">
+                <Text mb="48px" size="16px" weight="400">
                     Здесь мы будем творить создавать нашу шахматную доску.
                 </Text>
+
+                <ChessBoard mw="700px" />
             </Base>
         </Container>
     ); 
